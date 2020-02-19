0ecd83893c834832aee257af66c1a8f9f196032d
Tanuja Mandiye <tanuja876mandiye@gmail.com>
2020-02-19 12:49:53 +0530

    projects

---
+++ mysql5.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try 
+{
+$conn = new PDO("mysql:host= $servername",$username , $password);
+// Set the PDO error mode to exception
+$conn->stAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql= "CREATE DATABASE myDBPDO";
+// use exec() because no results are returned
+$conn->exec($sql);
+echo "Database created successfully<br>";
+}
+catch (PDOException $e)
+{
+    echo $sql ."<br>" . $e->grtmessage();
+}
+$conn = null;
+?>
